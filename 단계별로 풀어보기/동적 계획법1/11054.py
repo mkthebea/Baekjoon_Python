06b8a7935cdd2958effc9e44b57691e64b7db3e0
mkthebea <5596molly@naver.com>
2022-11-20 17:26:51 +0900

    가장 긴 바이토닉 부분 수열

--- 단계별로 풀어보기/동적 계획법1/11054.py
+++ 단계별로 풀어보기/동적 계획법1/11054.py
@@ -3,7 +3,9 @@ a = list(map(int, input().split()))
 left = [0] * n
 left[0] = 1
 for i in range(1, n):
-    smaller = list(filter(lambda x : x < a[i], a[0:i]))
+    tem = a[0:i]
+    # a[i] > a[j]인 j에 대해 left[j]들을 모아놓은 리스트 필요
+    smaller = [left[j] for j in range(i) if a[j] < a[i]]
     if len(smaller) == 0:
         left[i] = 1
     else:
@@ -12,11 +14,14 @@ for i in range(1, n):
 right = [0] * n
 right[-1] = 1
 for i in range(n-2, -1, -1):
-    smaller = list(filter(lambda x : x < a[i], a[i + 1:n]))
+    tem = a[i+1:n]
+    # a[i] > a[j]인 j에 대해 right[j]들을 모아놓은 리스트 필요
+    smaller = [right[j] for j in range(i+1, n) if a[j] < a[i]]
     if len(smaller) == 0:
         right[i] = 1
     else:
         right[i] = max(smaller) + 1
+
 res = [0] * n
 for i in range(n):
     res[i] = left[i] + right[i]
