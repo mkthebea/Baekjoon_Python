459d25e93eb285019e5ff24c5a9a69da77fa98e5
mkthebea <5596molly@naver.com>
2023-02-16 18:53:18 +0900

    알고리즘 수업 - 깊이 우선 탐색 1

---
+++ 단계별로 풀어보기/그래프와 순회/24479.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+def dfs(graph, v, visited):
+    global count
+    visited[v] = count
+    count += 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(graph, i, visited)
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n + 1)
+count = 1
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for i in graph:
+    i.sort()
+
+dfs(graph, r, visited)
+for i in range(1, n+1):
+    print(visited[i])
