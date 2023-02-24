54610fe48d9a859e54dee67b3fdbf38b8bbd0aae
mkthebea <5596molly@naver.com>
2023-02-25 03:55:23 +0900

    N과 M (1)

--- 단계별로 풀어보기/백트래킹/15649.py
+++ 단계별로 풀어보기/백트래킹/15649.py
@@ -1,15 +1,6 @@
+from itertools import permutations
+
 n, m = map(int, input().split())
-
-# nCm 구하기
-ncm = []
-for i in range(1,n+1):
-    tem = list(range(i,i+m))
-    ncm.append(tem)
-    # for j in range(1,m+1):
-    #     tem = list(range(i,i+j))
-    #     ncm.append(tem)
-
-print(ncm)
-
-
-# print(list(range(1, n + 1)))
+num = [i+1 for i in range(n)]
+for i in permutations(num, m):
+    print(' '.join(map(str,i)))
