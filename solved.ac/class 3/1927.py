cb390c6d8cb05523f92c7690ebc18b3db1a55600
mkthebea <5596molly@naver.com>
2023-03-09 23:13:51 +0900

    최소 힙

---
+++ solved.ac/class 3/1927.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+heap = []
+
+n = int(input())
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, x)
